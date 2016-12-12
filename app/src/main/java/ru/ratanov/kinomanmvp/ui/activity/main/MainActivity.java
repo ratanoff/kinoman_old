795aaa9484aec3420d851f3a8e0db2edd8142345
Leonid <leonid.ratanoff@gmail.com>
2016-12-13 01:09:29 +0500

    Added SearchView to MainActivity (dummy yet)

--- app/src/main/java/ru/ratanov/kinomanmvp/ui/activity/main/MainActivity.java
+++ app/src/main/java/ru/ratanov/kinomanmvp/ui/activity/main/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import ru.ratanov.kinomanmvp.R;
 import ru.ratanov.kinomanmvp.ui.fragment.main.FilmsFragment;
@@ -14,6 +16,7 @@ import ru.ratanov.kinomanmvp.ui.fragment.main.MultsFragment;
 import ru.ratanov.kinomanmvp.ui.fragment.main.SerialsFragment;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.lapism.searchview.SearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +24,7 @@ import java.util.List;
 public class MainActivity extends MvpAppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,23 @@ public class MainActivity extends MvpAppCompatActivity {
         // Set Tabs inside Toolbar
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+        //
+        setupSearchView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_search) {
+            toggleSearch();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     // Add Fragments to Tabs
@@ -47,6 +68,22 @@ public class MainActivity extends MvpAppCompatActivity {
         adapter.addFragment(new SerialsFragment(), "Сериалы");
 
         viewPager.setAdapter(adapter);
+    }
+
+    void setupSearchView() {
+        mSearchView = (SearchView) findViewById(R.id.searchview);
+        mSearchView.setHint(R.string.search);
+        mSearchView.setVersion(SearchView.VERSION_MENU_ITEM);
+        mSearchView.setVersionMargins(SearchView.VERSION_MARGINS_MENU_ITEM);
+        mSearchView.setTheme(SearchView.THEME_LIGHT);
+    }
+
+    void toggleSearch() {
+        if (mSearchView.isSearchOpen()) {
+            mSearchView.close(true);
+        } else {
+            mSearchView.open(true);
+        }
     }
 
     // Adapter for ViewPager
